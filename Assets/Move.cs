69f1cebadcf8b298204ce2d9e1472bcb35f50769
Derek Partible <dpartible92@gmail.com>
2014-11-03 23:31:18 -0700

    init 2

---
+++ Assets/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public Vector3 direction = Vector3.forward; // 'forward' ==> on z axis
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	// Called when object is created
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// normalized speed 
+		transform.position += transform.rotation * (direction.normalized * speed * Time.deltaTime);
+	}
+}
