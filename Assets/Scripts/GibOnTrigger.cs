6e8b970735e71c3433d3aaa6d23a001d27691160
Derek Partible <dpartible92@gmail.com>
2014-11-04 00:09:24 -0700

    end 2

---
+++ Assets/Scripts/GibOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GibOnTrigger : MonoBehaviour {
+
+	// put something in place of destroyed object
+	public GameObject gib;
+
+	void OnTriggerEnter(){	// object runs into something else
+		Instantiate (gib, transform.position, Quaternion.identity); // 'Quaternion.identity' ==> dont care about rotation
+		Destroy (gameObject);	// destroy object this script is attached to
+	}
+
+}
